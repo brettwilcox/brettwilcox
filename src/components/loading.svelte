d8aa9d8a0071bf0c8a5f4b50338925352fe7fd5a
Brett Wilcox <brett@scorpion.io>
2021-07-13 23:26:19 -0500

    Storybook stopped working...

--- src/components/loading.svelte
+++ src/components/loading.svelte
@@ -1,1 +1,14 @@
-<h1>Hello world!</h1>
+<div
+	class="border border-blue-300 shadow rounded-md p-4 max-w-sm w-full mx-auto"
+>
+	<div class="animate-pulse flex space-x-4">
+		<div class="rounded-full bg-blue-400 h-12 w-12" />
+		<div class="flex-1 space-y-4 py-1">
+			<div class="h-4 bg-blue-400 rounded w-3/4" />
+			<div class="space-y-2">
+				<div class="h-4 bg-blue-400 rounded" />
+				<div class="h-4 bg-blue-400 rounded w-5/6" />
+			</div>
+		</div>
+	</div>
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,9 +3,9 @@
 </svelte:head>
 
 <div class="bg-white dark:bg-gray-800">
-	<p class="text-gray-600 dark:text-gray-300">Coming soon.</p>
+	<p class="text-gray-600 dark:text-gray-300">This site will be coming soon!</p>
 </div>
 
 <div>
-	<p><a href="https://github.com/sim-shell/sim">GitHub</a></p>
+	<p><a href="https://github.com/brettwilcox/brettwilcox">GitHub</a></p>
 </div>
